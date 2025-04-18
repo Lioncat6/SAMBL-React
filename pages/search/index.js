91686f577a78348093099297eba2a09d6fb12181
Lioncat6 <lioncat6pmc@gmail.com>
2025-04-18 17:34:18 -0500

    Add title to certain pages

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,12 +1,12 @@
 import { useRouter } from "next/router";
 import ItemList from "../../components/ItemList";
-
+import Head from 'next/head';
 async function getItems(query) {
     const response = await fetch(`http://localhost:3000/api/searchArtists?query=${query}`);
     if (response.ok) {
         const data = await response.json();
         console.log("Response Data:", data);
-        return data; 
+        return data;
     } else {
         throw new Error("Error fetching artist data");
     }
@@ -19,7 +19,7 @@ export async function getServerSideProps(context) {
         const items = await getItems(query);
         console.log(items);
         return {
-            props: { items }, 
+            props: { items },
         };
     } catch (error) {
         console.error("Error fetching artist data:", error);
@@ -29,21 +29,25 @@ export async function getServerSideProps(context) {
     }
 }
 
-export default function search({items}) {
+export default function search({ items }) {
     const router = useRouter();
     const { query } = router.query;
     return (
         <>
-                <div id="err" />
-                <div className="titleContainer">
-                    <h1 id="searchFor">Search Results for "{query}"</h1>
-                </div>
-                <div id="contentContainer">
-                    <div id="loadingContainer" />
-                    <div id="loadingText" />
-                        <ItemList type={"artist"} items={items} />
-                        <div id="statusText" />
-                </div>
+            <Head>
+                <title>SAMBL • Resutls for  "{query}"</title>
+                <meta name="description" content={`SAMBL • Search results for "${query}"`} />
+            </Head>
+            <div id="err" />
+            <div className="titleContainer">
+                <h1 id="searchFor">Search Results for "{query}"</h1>
+            </div>
+            <div id="contentContainer">
+                <div id="loadingContainer" />
+                <div id="loadingText" />
+                <ItemList type={"artist"} items={items} />
+                <div id="statusText" />
+            </div>
 
         </>
 
