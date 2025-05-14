40b56908c4b5ed9e0390c8c1f489d00e50389dd1
Lioncat6 <lioncat6pmc@gmail.com>
2025-05-14 14:29:23 -0500

    Fixed issue with exporting artists

--- components/Popup.js
+++ components/Popup.js
@@ -178,14 +178,14 @@ function ExportMenu({ data, close }) {
 							<div className={styles.property}>
 								<button
 									className={styles.copyButton}
-									onClick={() => handleCopy(String(value))}
+									onClick={() => handleCopy(Array.isArray(value) && typeof value[0] === "object" ? JSON.stringify(value, null, 2) : String(value))}
 									title="Copy to Clipboard"
 								>
 									<FaCopy />
 								</button>{" "}
 								{key}
 							</div>
-							<div className={styles.propertyData}>{String(value)}</div>
+							<div className={styles.propertyData}>{Array.isArray(value) && typeof value[0] === "object" ? JSON.stringify(value, null, 2) : String(value)}</div>
 						</div>
 					);
 				})}
