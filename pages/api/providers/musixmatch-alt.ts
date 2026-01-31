f68b7f717c15fe6e58ed323814a6fc12c9f8462d
Lioncat6 <lioncat6pmc@gmail.com>
2026-01-31 17:47:46 -0600

    Refactor musixmatch code to typescript

--- pages/api/providers/musixmatch-alt.ts
+++ pages/api/providers/musixmatch-alt.ts
@@ -2,49 +2,55 @@ import MusixMatchAPI from "./lib/musixmatch-alt";
 import logger from "../../../utils/logger";
 import withCache from "../../../utils/cache";
 import ErrorHandler from "../../../utils/errorHandler";
+import { MusixMatchProvider, Provider } from "../../../types/provider-types";
+import { AxiosBasicCredentials, AxiosProxyConfig } from "axios";
 const namespace = "musixmatch";
 
 const err = new ErrorHandler(namespace);
 
-let proxies = {};
+let proxy: AxiosProxyConfig | null = null;
 if (
 	process.env.MXM_PROXY_HOST &&
 	process.env.MXM_PROXY_PORT
 ) {
-	proxies = {
-		protocol: 'http',
-		host: process.env.MXM_PROXY_HOST,
-		port: parseInt(process.env.MXM_PROXY_PORT, 10),
-		auth: {
+	let auth: AxiosBasicCredentials | undefined = undefined;
+	if (process.env.MXM_PROXY_USERNAME && process.env.MXM_PROXY_PASSWORD) {
+		auth = {
 			username: process.env.MXM_PROXY_USERNAME,
 			password: process.env.MXM_PROXY_PASSWORD
 		}
+	}
+	proxy = {
+		protocol: 'http',
+		host: process.env.MXM_PROXY_HOST,
+		port: parseInt(process.env.MXM_PROXY_PORT, 10),
+		auth
 	};
 }
 
 const hostname = process.env.MUSIXMATCH_HOSTNAME || "https://www.musixmatch.com";
 
-function initMxM(proxies) { 
-	if (proxies != {}) {
-		return new MusixMatchAPI(proxies, process.env.MUSIXMATCH_API_KEY, hostname);
+function initMxM(proxy?: AxiosProxyConfig | null) { 
+	if (proxy) {
+		return new MusixMatchAPI(proxy, process.env.MUSIXMATCH_API_KEY, hostname);
 	} else {
 		return new MusixMatchAPI(null, process.env.MUSIXMATCH_API_KEY, hostname);
 	}
 }
 
-let mxmAPI = initMxM(proxies);
+let mxmAPI = initMxM(proxy);
 let lastRefreshed = Date.now();
 
 async function refreshApi() {
     const timeout = 60 * 60 * 1000 * 12;
     if (Date.now() - lastRefreshed > timeout) {
-        mxmAPI = initMxM(proxies);
+        mxmAPI = initMxM(proxy);
         lastRefreshed = Date.now();
         logger.debug("MusixMatch Alt API refreshed");
     }
 }
 
-async function withRetry(apiCall, retries = 3, delay = 250) {
+async function withRetry(apiCall: () => Promise<any>, retries = 3, delay = 250) {
 	for (let attempt = 1; attempt <= retries; attempt++) {
 		try {
 			return await apiCall();
@@ -60,7 +66,7 @@ async function withRetry(apiCall, retries = 3, delay = 250) {
 	}
 }
 
-async function getTrackByISRC(isrc) {
+async function getTrackByISRC(isrc: string) {
 	try {
 
 		await refreshApi();
@@ -72,9 +78,9 @@ async function getTrackByISRC(isrc) {
 			}
 			return trackData.message.body;
 		} else {
-			if (trackData.message.header?.status_code === 401) {
-				logger.warn(`Recieved 401 from MusixMatch. Reason: ${trackData.message.header?.hint}`);
-				throw new Error(`Recieved 401 from MusixMatch. Reason: ${trackData.message.header?.hint}`);
+			if (trackData.message?.header?.status_code === 401) {
+				logger.warn(`Recieved 401 from MusixMatch. Reason: ${trackData.message.header?.hint || "Unknown"}`);
+				throw new Error(`Recieved 401 from MusixMatch. Reason: ${trackData.message.header?.hint || "Unknown"}`);
 			}
 			return null;
 		}
@@ -86,7 +92,7 @@ async function getTrackByISRC(isrc) {
 
 
 
-const musixmatch = {
+const musixmatch: MusixMatchProvider = {
 	namespace,
 	getTrackByISRC: withCache(getTrackByISRC, { ttl: 60 * 10,  namespace: namespace }),
 };
