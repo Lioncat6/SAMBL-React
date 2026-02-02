fdec06446b4acdec015ca063c6ef1f93b281f613
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2026-02-01 21:27:37 -0600

    Update search page to ts and implement error handling

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -2,37 +2,47 @@ import { useRouter } from "next/router";
 import ItemList from "../../components/ItemList";
 import Head from 'next/head';
 import SearchBox from '../../components/SearchBox';
+import { ArtistSearchData } from "../../types/api-types";
+import { SAMBLError } from "../../types/component-types";
+import ErrorPage from "../../components/ErrorPage";
 
 async function getItems(query, provider) {
     const response = await fetch(`http://localhost:${process.env.PORT || 3000}/api/searchArtists?query=${query}&provider=${provider}`);
     if (response.ok) {
-        const data = await response.json();
-        return data; 
+        const data = await response.json() as ArtistSearchData;
+        return data;
     } else {
-        throw new Error("Error fetching artist data", response.statusText);
+        throw new Error("Error fetching artist data: " + response.statusText);
     }
 }
 
 export async function getServerSideProps(context) {
-    let { query, provider } = context.query;
-    if (!provider) {
-        provider = context.req.cookies?.provider || "spotify";
-    }
-
     try {
+        let { query, provider } = context.query;
+        if (!provider) {
+            provider = context.req.cookies?.provider || "spotify";
+        }
         const items = await getItems(query, provider);
         return {
             props: { items },
         };
     } catch (error) {
-        console.error("Error fetching artist data:", error);
+        const samblError: SAMBLError = {
+            type: "general",
+            message: String(error)
+        }
         return {
-            notFound: true,
+            props: { error: samblError }
         };
     }
 }
 
-export default function search({ items }) {
+export default function search({ items, error }) {
+    if (error || !items) {
+        return (
+            <ErrorPage error={error} />
+        )
+    }
     const router = useRouter();
     const { query } = router.query;
     return (
@@ -50,7 +60,7 @@ export default function search({ items }) {
             <div id="contentContainer">
                 <div id="loadingContainer" />
                 <div id="loadingText" />
-                    <ItemList type={"artist"} items={items} />
+                <ItemList type={"artist"} items={items} />
                 <div id="statusText" />
             </div>
 
