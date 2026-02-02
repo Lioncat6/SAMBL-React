817357a50cd8b67c666c7b9ac8210ed29efdcbe8
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2026-02-01 23:03:37 -0600

    (buttons) Refactor to ts

--- components/buttons.tsx
+++ components/buttons.tsx
@@ -3,62 +3,30 @@ import Link from "next/link";
 import text from "../utils/text";
 import editNoteBuilder from "../utils/editNoteBuilder";
 import { Button } from "@headlessui/react";
-import { toast, Flip } from "react-toastify";
+import toasts from "../utils/toasts";
 
 async function deepSearch(url) {
-	dispError("Please double check deep searches before submitting edits!", "warn")
+	toasts.warn("Please double check deep searches before submitting edits!")
 	try {
-		const response = await dispPromise(fetch(`/api/artistDeepSearch?url=${encodeURIComponent(url)}`), "Running Deep Search...");
+		const response = await toasts.dispPromise(fetch(`/api/artistDeepSearch?url=${encodeURIComponent(url)}`), "Running Deep Search...", "Deep Search failed!");
 		if (response.ok) {
+			console.log("a")
 			let data = await response.json();
 			let editNote = editNoteBuilder.buildDeepSearchEditNote(data);
 			let editUrl = `https://musicbrainz.org/artist/${data.mbid}/edit?edit-artist.url.0.text=${url}&edit-artist.url.0.link_type_id=194&edit-artist.edit_note=${editNote}`;
 			if (data.nameSimilarity < 0.30) {
-				dispError(`Artist name too different for match! (${Math.round(data.nameSimilarity * 100)}% - ${data.mbName})`)
+				toasts.error(`Artist name too different for match! (${Math.round(data.nameSimilarity * 100)}% - ${data.mbName})`)
 				return;
 			}
 			window.open(editUrl, "_blank");
 		} else {
-			dispError((await response.json()).error);
+			toasts.error((await response.json()).error);
 		}
 	} catch (error) {
 		console.error(error);
-		dispError(error.message);
+		toasts.error(error.message);
 	}
 }
-
-let toastProperties = {
-	position: "top-left",
-	autoClose: 5000,
-	hideProgressBar: false,
-	closeOnClick: false,
-	pauseOnHover: true,
-	draggable: true,
-	progress: undefined,
-
-	transition: Flip,
-};
-async function dispError(message, type = "error") {
-	if (type === "error") {
-		toast.error(message, toastProperties);
-	} else {
-		toast.warn(message, toastProperties);
-	}
-}
-
-
-async function dispPromise(promise, message) {
-		return toast
-			.promise(
-				promise,
-				{
-					pending: message,
-					error: "Data not found!",
-				},
-				toastProperties
-			)
-			.finally(() => {});
-	}
 
 export default function AddButtons({ artist }) {
 	let editNote = editNoteBuilder.buildEditNote('Artist', artist.provider, artist.url, artist.url);
