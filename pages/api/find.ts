a7e29111f05f29954832fb5f0d5827f63b378dbb
Lioncat6 <lioncat6pmc@gmail.com>
2026-02-14 18:30:01 -0600

    Refactor and overhaul find API

---
+++ pages/api/find.ts
@@ -0,0 +1,73 @@
+import spotify from "./providers/spotify";
+import * as spotifyModule from "./providers/spotify";
+import musicbrainz from "./providers/musicbrainz";
+import musixmatch from "./providers/musixmatch";
+import deezer from "./providers/deezer";
+import tidal from "./providers/tidal";
+import logger from "../../utils/logger";
+import text from "../../utils/text";
+import applemusic from "./providers/applemusic";
+import { FindData, SAMBLApiError } from "../../types/api-types";
+import { AlbumObject, RawAlbumData, TrackObject } from "../../types/provider-types";
+import { NextApiRequest, NextApiResponse } from "next";
+import providers from "./providers/providers";
+import normalizeVars from "../../utils/normalizeVars";
+function createDataObject(source, imageUrl, title, artists, info, link, extraInfo = null) {
+	return {
+		source: source,
+		imageUrl: imageUrl,
+		title: title,
+		artists: artists,
+		info: info.filter((element) => element),
+		link: link,
+		extraInfo: extraInfo,
+	};
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	try {
+		const { query, type } = normalizeVars(req.query);
+		if (!query) {
+			return res.status(400).json({ error: "Parameter `query` is required" });
+		}
+		if (!type) {
+			return res.status(400).json({ error: "Parameter `type` is required" });
+		}
+		let resultItems = [];
+		let issues: SAMBLApiError[] = [];
+
+		if (type.toLocaleLowerCase() == "upc") {
+			let albums: AlbumObject[] = []
+			const providerList = providers.getAllProviders(["getAlbumByUPC"]);
+			for (const provider of providerList){
+				try {
+					const rawData = await provider.getAlbumByUPC(query)
+					if (rawData) rawData.forEach((album) => albums.push(album))
+				} catch (error) {
+					logger.error(error)
+					issues.push(<SAMBLApiError>{provider: provider.namespace, error: error.message || error.toString()})
+				}
+			}
+			return res.status(200).json({type: "UPC", data: albums, issues} as FindData)
+
+		} else if (type.toLocaleLowerCase() == "isrc") {
+			let tracks: TrackObject[] = []
+			const providerList = providers.getAllProviders(["getTrackByISRC"]);
+			for (const provider of providerList){
+				try {
+					const rawData = await provider.getTrackByISRC(query)
+					if (rawData) rawData.forEach((track) => tracks.push(track))
+				} catch (error) {
+					logger.error(error)
+					issues.push(<SAMBLApiError>{provider: provider.namespace, error: error.message || error.toString()})
+				}
+			}			
+			return res.status(200).json({type: "UPC", data: tracks, issues} as FindData)
+		} else {
+			return res.status(400).json({ error: "Invalid query type!" } as SAMBLApiError);
+		}
+	} catch (error) {
+		logger.error("Error in find API", error);
+		res.status(500).json({ error: "Internal Server Error", details: error.message } as SAMBLApiError);
+	}
+}
