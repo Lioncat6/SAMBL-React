b59f5763ddfe192c4b1e60bf5d430c5a1f00ded9
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2026-03-10 05:33:47 -0500

    Fix footer keys issue

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/footer.module.css'
 const links: { name: string, href: string }[] = [
 	{ name: "Github", href: "https://github.com/Lioncat6/SAMBL-React" },
 	{ name: "MetaBrainz", href: "https://community.metabrainz.org/t/sambl-streaming-artist-musicbrainz-lookup/716550" },
-	{ name: "Donate", href: "https://ko-fi.com/lioncat6"}
+	{ name: "Donate", href: "https://ko-fi.com/lioncat6" }
 ]
 
 export default function footer() {
@@ -13,14 +13,14 @@ export default function footer() {
 				<div>
 					{/* <div className={styles.footerText}>Please report any bugs or suggestions here{" "}</div> */}
 					{links.map((link, index) => (
-						<>
+						<span key={index}>
 							{index > 0 && (
 								<>{" "}|{" "}</>
 							)}
 							<a rel="noopener" href={link.href} target="_blank" key={index}>
 								{link.name}
-							</a>	
-						</>
+							</a>
+						</span>
 					))}
 
 					{/* <div>
