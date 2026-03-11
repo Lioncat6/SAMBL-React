5635e2f54e2c7367507ff703877cf304122f2e9e
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2026-03-10 21:57:32 -0500

    Fix MET workaround to not interfere with other providers

--- lib/seeders/met.ts
+++ lib/seeders/met.ts
@@ -1,13 +1,16 @@
 import { Seeder } from "../../types/seeder-types";
 import parsers from "../parsers/parsers";
 
-function generateFakeUrl(url){
-    const id = parsers.getUrlInfo(url)?.id || "";
-    return `https://www.qobuz.com/us-en/album/placeholder/${id}`
+function getUrl(url: string){
+    const urlInfo = parsers.getUrlInfo(url);
+    if (urlInfo?.provider == "qobuz"){
+        return `https://www.qobuz.com/us-en/album/placeholder/${urlInfo.id}`
+    }
+    return url;
 }
 
 function buildUrl(url: string, upc?: string | null): string {
-    return `https://seed.musichoarders.xyz/?identifier=${generateFakeUrl(url)}`
+    return `https://seed.musichoarders.xyz/?identifier=${getUrl(url)}`
 }
 
 const met: Seeder = {
