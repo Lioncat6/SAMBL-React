91cb498eba9041f42e2bb7271057b2cc17662da3
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2026-05-07 02:08:34 -0500

    Refactor layout for a smaller header + Lion's changes
    Co-authored-by: RustyNova <50844553+RustyNova016@users.noreply.github.com>

--- components/ProviderPill.tsx
+++ components/ProviderPill.tsx
@@ -2,7 +2,7 @@ import { FaSpotify, FaDeezer, FaSoundcloud } from "react-icons/fa6";
 import { SiTidal, SiBandcamp, SiApplemusic, SiNaver } from "react-icons/si";
 import { useState, useEffect } from "react";
 import { SAMBLSettingsContext, useSettings } from "./SettingsContext";
-import styles from "../styles/ProviderPill.module.css";
+import styles from "../styles/ProviderPill.module.scss";
 import { ProviderDisplay } from "../types/component-types";
 import { ProviderNamespace } from "../types/provider-types";
 import clientProviders from "../utils/clientProviders";
@@ -53,13 +53,13 @@ export default function ProviderPill() {
     useEffect(() => {
         if (!loading) {
             setCurrentProvider(settings.currentProvider);
-			const params = new URLSearchParams(window.location.search);
-			if (window.location.pathname === "/search" && params.has("provider")) {
-				const providerParam = params.get("provider");
-				if (providerArray.some((p) => p.namespace === providerParam)) {
-					handleSelect(providerParam as ProviderNamespace);
-				}
-			}
+            const params = new URLSearchParams(window.location.search);
+            if (window.location.pathname === "/search" && params.has("provider")) {
+                const providerParam = params.get("provider");
+                if (providerArray.some((p) => p.namespace === providerParam)) {
+                    handleSelect(providerParam as ProviderNamespace);
+                }
+            }
         }
     }, [loading]);
 
@@ -76,7 +76,15 @@ export default function ProviderPill() {
 
     return (
         <div className={styles.ProviderPillContainer}>
-            <div className={styles.ProviderPill} style={{ position: "absolute" }}>
+            <div className={styles.ProviderPill}>
+
+                <div
+                    className={`${styles.SelectedProvider} ${styles[currentProvider]}`}
+                    style={{
+                        left: `${selectedIndex * 30}px`,
+                    }}
+                />
+
                 {providerArray.map((element) => (
                     <button
                         className={`${styles.provider} ${styles[element.namespace]} ${currentProvider === element.namespace ? styles.selected : ""}`}
@@ -87,12 +95,7 @@ export default function ProviderPill() {
                         {element.icon}
                     </button>
                 ))}
-                <div
-                    className={`${styles.SelectedProvider} ${styles[currentProvider]}`}
-                    style={{
-                        left: `${4 + selectedIndex * 30}px`,
-                    }}
-                />
+
             </div>
         </div>
     );
--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import styles from "../styles/header.module.css";
+import styles from "../styles/header.module.scss";
 import dynamic from "next/dynamic";
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 
@@ -11,68 +11,86 @@ import ProviderPill from "./ProviderPill";
 
 import { useExportData } from "./Export";
 
-import Popup from "./Popup"
+import Popup from "./Popup";
 import ConfigureMenuPopup from "./Popups/ConfigureMenu";
+import Image from "next/image";
 
 // const Popup = dynamic(() => import("./Popup"), { ssr: false });
 
 export default function Header() {
-	const { exportItems, exportAllItems } = useExportData();
+
 
 	return (
 		<>
-			<header className={styles.header}>
+			<header className={styles.header_container}>
 				<ProviderPill />
-				<Link className={styles.samblWrapper} href="/">
-					<div className={styles.imagewrapper}>
-						<img src="assets/images/favicon.svg" alt="SAMBL Logo" className={styles.logo} />
-					</div>
-				</Link>
-				<div className={styles.textwrapper}>
-					<h1>SAMBL</h1>
-					<div className={styles.subdesc}>Streaming Artist MusicBrainz Lookup</div>
-				</div>
-				<div className={styles.buttonWrapper}>
-					<Menu as="div" className={styles.dropdownWrapper}>
-						<MenuButton className={styles.toolsButton}>
-							<div className={styles.buttonText}>
-								<FaTools /><div className={styles.collapse}> Tools</div>
-							</div>
-						</MenuButton>
-						<MenuItems className={styles.dropdownMenu} transition anchor="bottom end">
-							<MenuItem>
-								<div className={styles.menuItem} onClick={exportItems}>
-									<TbPackageExport /> Export Items
-								</div>
-							</MenuItem>
-							<MenuItem>
-								<div className={styles.menuItem} onClick={exportAllItems}>
-									<TbTableExport /> Export All Items
-								</div>
-							</MenuItem>
-							<MenuItem>
-								<Link className={styles.activeItem} href="/find">
-									<FaMagnifyingGlass /> Find
-								</Link>
-							</MenuItem>
-						</MenuItems>
-					</Menu>
-					<button className={styles.savedArtists}>
-						<div className={styles.buttonText}>
-							<FaUser /><div className={styles.collapse}> Artists</div>
-						</div>
-					</button>
-					<ConfigureMenuPopup
-						button={
-							<button className={styles.configButton}>
-								<div className={styles.buttonText}>
-									<FaGear /><div className={styles.collapse}> Configure</div>
-								</div>
-							</button>
-						}
-					/>
-				</div>
+				<Title />
+				<Buttons />
 			</header>
 		</>
 	);
+}
+
+function Title() {
+	return <>
+		<div className={styles.title_container}>
+			<Link href="/" >
+				<div className={styles.imagewrapper}>
+					{/* <img src="assets/images/favicon.svg" alt="SAMBL Logo" className={styles.logo} /> */}
+				<Image src="assets/images/favicon.svg" alt="SAMBL Logo" width={256} height={256} className={styles.logo} />
+				</div>
+			</Link>
+			<div className={styles.textwrapper}>
+				<h1 className={styles.title}>SAMBL</h1>
+				<div className={styles.subtitle}><b>S</b>treaming <b>A</b>rtist <b>M</b>usic<b>B</b>rainz <b>L</b>ookup</div>
+			</div>
+		</div>
+	</>;
+}
+
+function Buttons() {
+	const { exportItems, exportAllItems } = useExportData();
+
+	return <>
+		<div className={styles.button_container}>
+			<Menu as="div" className={styles.dropdownWrapper}>
+				<MenuButton className={styles.toolsButton}>
+					<div className={styles.buttonText}>
+						<FaTools /><div className={styles.collapse}> Tools</div>
+					</div>
+				</MenuButton>
+				<MenuItems className={styles.dropdownMenu} transition anchor="bottom end">
+					<MenuItem>
+						<div className={styles.menuItem} onClick={exportItems}>
+							<TbPackageExport /> Export Items
+						</div>
+					</MenuItem>
+					<MenuItem>
+						<div className={styles.menuItem} onClick={exportAllItems}>
+							<TbTableExport /> Export All Items
+						</div>
+					</MenuItem>
+					<MenuItem>
+						<Link className={styles.activeItem} href="/find">
+							<FaMagnifyingGlass /> Find
+						</Link>
+					</MenuItem>
+				</MenuItems>
+			</Menu>
+			<button className={styles.savedArtists}>
+				<div className={styles.buttonText}>
+					<FaUser /><div className={styles.collapse}> Artists</div>
+				</div>
+			</button>
+			<ConfigureMenuPopup
+				button={
+					<button className={styles.configButton}>
+						<div className={styles.buttonText}>
+							<FaGear /><div className={styles.collapse}> Configure</div>
+						</div>
+					</button>
+				}
+			/>
+		</div>
+	</>;
 }
