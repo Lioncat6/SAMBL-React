4343b413cf1dfec2650ccd5c1b85733f959f783b
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2026-05-10 00:49:47 -0500

    (Discogs) Rework album fetching

--- lib/providers/discogs.ts
+++ lib/providers/discogs.ts
@@ -7,7 +7,7 @@ import text from "../../utils/text";
 import withCache from "../../utils/cache";
 import ErrorHandler from "../../utils/errorHandler";
 import parsers from "../parsers/parsers";
-import { Artist, DiscogsClient, GetArtistReleasesResponses, GetArtistResponse, GetMasterVersionsResponse, GetReleaseResponse, Label, PaginationResponse, SearchResponse, SearchResult } from '@lionralfs/discogs-client'
+import { Artist, DiscogsClient, Format, GetArtistReleasesResponses, GetArtistResponse, GetMasterVersionsResponse, GetReleaseResponse, Label, PaginationResponse, SearchResponse, SearchResult } from '@lionralfs/discogs-client'
 import { create } from "node:domain";
 import { release } from "node:os";
 import { get } from "node:http";
@@ -131,76 +131,163 @@ function formatArtistLookupData(rawData: GetArtistResponse): GetArtistResponse {
 	return rawData;
 }
 
-type ArtistAlbumsResponse = GetArtistReleasesResponses & PaginationResponse & {
+type ArtistAlbumsResponse = {
+	mainAlbums: (SearchResponse & PaginationResponse) | null,
+	featuredAlbums: (SearchResponse & PaginationResponse) | null,
+	releases: (GetArtistReleasesResponses & PaginationResponse) | null,
 	albumArtist: ArtistObject | null;
 };
 
-async function getMasterReleases(master: GetArtistReleasesResponses['releases'][number]): Promise<GetArtistReleasesResponses['releases'] | null> {
-	console.log(`Fetching master releases for master ID ${master.id}...`);
-	try {
-		const data = await discogsDB.getMasterVersions(master.id);
-		if (data && data.data && data.data.versions) {
-			let moddedReleases: GetArtistReleasesResponses['releases'] = []
-			for (const release of data.data.versions) {
-				moddedReleases.push({ 
-					...release, 
-					artist: master.artist, 
-					main_release: master.main_release, 
-					role: master.role, 
-					type: 'release', 
-					year: master.year });
-			}
-			return moddedReleases;
-		}
-	} catch (error) {
-		err.handleError("Error fetching master releases:", error);
-		return null;
-	}
-	return null;
-}
+// async function getMasterReleases(master: GetArtistReleasesResponses['releases'][number]): Promise<GetArtistReleasesResponses['releases'] | null> {
+// 	console.log(`Fetching master releases for master ID ${master.id}...`);
+// 	try {
+// 		const data = await discogsDB.getMasterVersions(master.id);
+// 		if (data && data.data && data.data.versions) {
+// 			let moddedReleases: GetArtistReleasesResponses['releases'] = []
+// 			for (const release of data.data.versions) {
+// 				moddedReleases.push({ 
+// 					...release, 
+// 					artist: master.artist, 
+// 					main_release: master.main_release, 
+// 					role: master.role, 
+// 					type: 'release', 
+// 					year: master.year });
+// 			}
+// 			return moddedReleases;
+// 		}
+// 	} catch (error) {
+// 		err.handleError("Error fetching master releases:", error);
+// 		return null;
+// 	}
+// 	return null;
+// }
 
-async function getArtistAlbums(artistId: string, offset: number = 0, limit: number = 25): Promise<ArtistAlbumsResponse | null> {
+type artistAlbumResult = (PartialDiscogsRelease | PartialDiscogsSearchResult)
+type artistAlbumsResults = artistAlbumResult[];
+
+async function getArtistAlbums(artistId: string, offset: string = "1:1", limit: number = 25): Promise<artistAlbumsResults | null> {
 	try {
 		const artistData = await discogs.getArtistById(artistId);
 		let formattedArtistData: ArtistObject | null = null;
 		if (artistData) {
 			formattedArtistData = formatArtistObject(artistData);
+		} else {
+			return null;
 		}
-		let data = await discogsDB.getArtistReleases(artistId, { per_page: limit, page: Math.floor(offset / limit) + 1 });
-		let releases = data.data.releases;
-		let masters = releases.filter(release => release.type === "master");
-		let masterReleases: GetArtistReleasesResponses['releases'] = [];
-		for (let i = 0; i < masters.length; i++) {
-			const master = masters[i];
-			const masterVersions = await getMasterReleases(master);
-			if (masterVersions) {
-				masterReleases.push(...masterVersions);
-			}
-			if (i < masters.length - 1) {
-				await new Promise(resolve => setTimeout(resolve, 250));
+		// let data = await discogsDB.getArtistReleases(artistId, { per_page: limit, page: Math.floor(offset / limit) + 1 });
+		// let releases = data.data.releases;
+		// let masters = releases.filter(release => release.type === "master");
+		// let masterReleases: GetArtistReleasesResponses['releases'] = [];
+		// for (let i = 0; i < masters.length; i++) {
+		// 	const master = masters[i];
+		// 	const masterVersions = await getMasterReleases(master);
+		// 	if (masterVersions) {
+		// 		masterReleases.push(...masterVersions);
+		// 	}
+		// 	if (i < masters.length - 1) {
+		// 		await new Promise(resolve => setTimeout(resolve, 250));
+		// 	}
+		// }
+		// data.data.releases.push(...masterReleases);
+		// 
+		// const pageData = offset.split(":");
+	
+		// if (pageData.length == 3) {
+		// 	mainPage = Number(pageData[0])
+		// 	featuredPage = Number(pageData[1])
+		// 	releasesPage = Number(pageData[2])
+		// }
+		// const mainData = mainPage > 0 ? await discogsDB.search({ artist: formattedArtistData.name, type: "release",  per_page: limit, page: mainPage }): null;
+		// const featuredData = featuredPage > 0 ? await discogsDB.search({ credit: formattedArtistData.name, type: "release",  per_page: limit, page: featuredPage }): null;
+		// const releases = releasesPage > 0 ? await discogsDB.getArtistReleases(artistId, {per_page: limit, page: featuredPage }) : null;
+
+		let mainAlbums: SearchResult[] = [];
+		let featuredAlbums: SearchResult[] = []; 
+		let releases: GetArtistReleasesResponses["releases"] = [];
+		let mainPage = 1;
+		let featuredPage = 1;
+		let releasesPage = 1;
+		const searchWithCache = withCache(discogsDB.search, { ttl: 60 * 60, namespace: namespace });
+		let mainDataResponse = await searchWithCache({ artist: formattedArtistData.name, type: "release",  per_page: 100, page: mainPage });
+		if (mainDataResponse?.data.results) {
+			mainAlbums.push(...mainDataResponse.data.results)
+		}
+		while (mainDataResponse.data.pagination.urls.next){
+			mainPage++;
+			mainDataResponse = await searchWithCache({ artist: formattedArtistData.name, type: "release",  per_page: 100, page: mainPage });
+			if (mainDataResponse?.data.results) {
+				mainAlbums.push(...mainDataResponse.data.results)
 			}
 		}
-		data.data.releases.push(...masterReleases);
-		return {
-			...data.data,
-			albumArtist: formattedArtistData
-		};
+		let featuredDataResponse = await searchWithCache({ credit: formattedArtistData.name, type: "release",  per_page: 100, page: featuredPage });
+		if (featuredDataResponse?.data.results) {
+			featuredAlbums.push(...featuredDataResponse.data.results)
+		}
+		while (featuredDataResponse.data.pagination.urls.next){
+			featuredPage++;
+			featuredDataResponse = await searchWithCache({ credit: formattedArtistData.name, type: "release",  per_page: 100, page: featuredPage });
+			if (featuredDataResponse?.data.results) {
+				featuredAlbums.push(...featuredDataResponse.data.results)
+			}
+		}
+		const getReleasesWithCache = withCache(discogsDB.getArtistReleases, { ttl: 60 * 60, namespace: namespace });
+		let releasesResponse = await getReleasesWithCache(artistId, {per_page: 100, page: releasesPage });
+		if (releasesResponse?.data.releases) {
+			releases.push(...releasesResponse?.data.releases)
+		}
+		while (releasesResponse.data.pagination.urls.next){
+			releasesPage++;
+			releasesResponse = await getReleasesWithCache(artistId, {per_page: 100, page: releasesPage });
+			if (releasesResponse?.data.releases) {
+				releases.push(...releasesResponse?.data.releases)
+			}
+		}
+		let releaseIdMap: Record<number, PartialDiscogsRelease> = {};
+		let masterIdMap: Record<number, PartialDiscogsRelease> = [];
+		let matchedIdArray: number[] = [];
+		for (let release of releases) {
+			if (release.type == "release") releaseIdMap[release.id] = release
+			if (release.type == "master") masterIdMap[release.id] = release
+		}
+		let finalAlbums: artistAlbumsResults = [];
+		for (let result of [...mainAlbums, ...featuredAlbums] as PartialDiscogsSearchResult[]){
+			if (Object.keys(releaseIdMap).includes(String(result.id)) || (result.master_id && Object.keys(masterIdMap).includes(String(result.master_id)))){
+				result.albumArtist = formattedArtistData;
+				result.release = releaseIdMap[result.id] || masterIdMap[result?.master_id || 'invalid'] || null
+				finalAlbums.push(result);
+				matchedIdArray.push(result.id);
+			}
+		}
+		for (let release of releases as PartialDiscogsRelease[]) {
+			if (release.type == "release") {
+				if (!matchedIdArray.includes(release.id)){
+					release.albumArtist = formattedArtistData;
+					finalAlbums.push(release);
+				}
+			}
+		}
+		return finalAlbums;
 	} catch (error) {
 		err.handleError("Error fetching artist albums:", error);
 		return null;
 	}
 }
 
-function formatAlbumGetData(rawData: ArtistAlbumsResponse): RawAlbumData {
+function formatAlbumGetData(rawData: artistAlbumsResults): RawAlbumData {
 	return {
-		albums: rawData.releases.filter(release => release.type === "release").map(release => ({ ...release, albumArtist: rawData.albumArtist })),
-		count: rawData.pagination.items,
-		current: rawData.pagination.page * rawData.pagination.per_page,
-		next: rawData.pagination.urls.next ? String((rawData.pagination.page) * rawData.pagination.per_page) : null
+		albums: rawData,
+		count: rawData.length,
+		current: 0,
+		next: null,
 	}
 }
 
 type PartialDiscogsRelease = GetArtistReleasesResponses["releases"][number] & {
+	albumArtist?: ArtistObject | null;
+};
+
+type PartialDiscogsSearchResult = SearchResult & {
+	release?: PartialDiscogsRelease | null;
 	albumArtist?: ArtistObject | null;
 };
 
@@ -277,7 +364,8 @@ function formatTrackObject(track: trackWithAlbum): TrackObject {
 	}
 }
 
-function getAlbumType(types: GetReleaseResponse["formats"]): string | null {
+function getAlbumType(types: GetReleaseResponse["formats"] | Format[] | undefined): string | null {
+	if (!types) return null;
 	const releaseTypeMap: Record<string, string> = {
 		'Album': 'Album',
 		'EP': 'EP',
@@ -301,7 +389,28 @@ function getAlbumType(types: GetReleaseResponse["formats"]): string | null {
 	return null;
 }
 
-function formatAlbumObject(album: PartialDiscogsRelease | GetReleaseResponse | GetMasterVersionsResponse["versions"][number]): AlbumObject {
+function findSearchResultBarcode(barcodes: SearchResult["barcode"]): string | null {
+	if (!barcodes) return null;
+	return barcodes.find((barcode) => /^\d+$/.test(barcode.trim())) || null
+}
+
+function getReleaseLabels(release: PartialDiscogsSearchResult): LabelObject[] {
+	let labels: LabelObject[] = []
+	release.label?.forEach((label) => {
+		labels.push({
+			provider: namespace,
+			type: 'label',
+			name: label,
+			id: null,
+			url: null,
+			catalogNumber: release.catno || undefined
+		})
+	})
+	return labels;
+}
+
+function formatAlbumObject(album: PartialDiscogsRelease | GetReleaseResponse | SearchResult): AlbumObject {
+	console.log(album)
 	if ("artist" in album) {
 		album = album as PartialDiscogsRelease;
 		return {
@@ -323,7 +432,7 @@ function formatAlbumObject(album: PartialDiscogsRelease | GetReleaseResponse | G
 			copyrights: null,
 			genres: null
 		}
-	} else {
+	} else if ("artists" in album) {
 		const fullalbum = album as GetReleaseResponse;
 		return {
 			type: "album",
@@ -344,8 +453,51 @@ function formatAlbumObject(album: PartialDiscogsRelease | GetReleaseResponse | G
 			genres: fullalbum.genres || null,
 			albumTracks: fullalbum.tracklist.map(track => formatTrackObject({ ...track, album: fullalbum })) || []
 		}
+	} else {
+		const fullalbum = album as PartialDiscogsSearchResult;
+		return {
+			type: "album",
+			provider: namespace,
+			id: String(fullalbum.id),
+			name: fullalbum.release?.title || fullalbum.title,
+			url: createUrl("album", String(fullalbum.id)),
+			albumArtists: (fullalbum.release?.role == 'Main' && fullalbum.albumArtist) ? [fullalbum.albumArtist] : [],
+			imageUrl: fullalbum.thumb || null,
+			imageUrlSmall: fullalbum.thumb || null,
+			artistNames: (fullalbum.release?.role == 'Main' && fullalbum.albumArtist) ? [fullalbum.albumArtist.name] : [],
+			releaseDate: fullalbum.year ? String(fullalbum.year) : null,
+			trackCount: null,
+			albumType: getAlbumType(fullalbum.formats),
+			upc: findSearchResultBarcode(fullalbum.barcode),
+			albumTracks: [],
+			labels: getReleaseLabels(fullalbum),
+			copyrights: null,
+			genres: fullalbum.genre || null
+		}
 	}
 }
+
+	// id: number;
+	// type: string;
+	// user_data: UserData;
+	// master_id?: number;
+	// master_url?: string;
+	// uri: string;
+	// title: string;
+	// thumb: string;
+	// cover_image: string;
+	// resource_url: string;
+	// country?: string;
+	// year?: string;
+	// format?: string[];
+	// label?: string[];
+	// genre?: string[];
+	// style?: string[];
+	// barcode?: string[];
+	// catno?: string;
+	// community?: Community;
+	// format_quantity?: number;
+	// formats?: Format[];
 
 async function getAlbumById(albumId: string): Promise<GetReleaseResponse | null> {
 	try {
