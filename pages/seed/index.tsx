5e54ef96ed2371ea1591054636ff86164b941bee
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2026-08-15 18:27:57 -0500

    Add some finishing touches to the Seeding page

--- pages/seed/index.tsx
+++ pages/seed/index.tsx
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import ItemList from "../../components/ItemList";
 import Head from 'next/head';
 import SearchBox from '../../components/SearchBox';
-import { ArtistSearchData, SAMBLApiError } from "../../types/api-types";
+import { APITimingData, ArtistSearchData, SAMBLApiError, SAMBLAPIResponse } from "../../types/api-types";
 import { SAMBLError } from "../../types/component-types";
 import ErrorPage from "../../components/ErrorPage";
 import SAMBLHead from "../../components/SAMBLHead";
@@ -17,25 +17,25 @@ import { ReleaseSeedButton } from "../../components/ReleaseSeed";
 
 async function getAlbum(url?: string, provider?: ProviderNamespace, artistId?: string, albumId?: string): Promise<AlbumStack | null> {
     // provider_id, provider, url, mbid, artist_id
-    let apiUrl = `http://localhost:${process.env.PORT || 3000}/api/compareSingleAlbum?artist_id=${artistId}&provider_id=${albumId}&provider=${provider}&fetchISRCs&resolveArtists`;
+    let apiUrl = `http://localhost:${process.env.PORT || 3000}/api/compareSingleAlbum?artist_id=${artistId}&provider_id=${albumId}&provider=${provider}&fetchISRCs&resolveArtists&detectLanguage`;
     if (url) {
-        apiUrl = `http://localhost:${process.env.PORT || 3000}/api/compareSingleAlbum?url=${url}&fetchISRCs&resolveArtists`;
+        apiUrl = `http://localhost:${process.env.PORT || 3000}/api/compareSingleAlbum?url=${url}&fetchISRCs&resolveArtists&detectLanguage`;
     }
     const response = await fetch(apiUrl);
     if (response.ok) {
-        const data = await response.json() as AlbumStack;
-        return data;
+        const data = await response.json() as SAMBLAPIResponse<AlbumStack>;
+        return data.data || null;
     } else {
         let errorMessage = "";
         try {
-            const errorJson = await response.json() as SAMBLApiError;
-            errorMessage = errorJson.details || errorJson.error;
+            const errorJson = (await response.json() as SAMBLAPIResponse<AlbumStack>).error;
+            errorMessage = errorJson?.details ?? errorJson?.error ?? response.statusText;
         } catch {
             errorMessage = response.statusText;
         }
         throw new Error(`Failed to fetch album data: ${errorMessage}`);
     }
-  
+
 }
 
 export async function getServerSideProps(context) {
@@ -72,7 +72,7 @@ export async function getServerSideProps(context) {
                 props: { error: samblError }
             };
         }
-        return { data: null }  
+        return { data: null }
     } catch (error) {
         const samblError: SAMBLError = {
             type: "general",
@@ -84,7 +84,7 @@ export async function getServerSideProps(context) {
     }
 }
 
-export default function Search({ data, error }: {data?: AlbumStack | null, error?:SAMBLError}) {
+export default function Seed({ data, error, timings }: { data?: AlbumStack | null, error?: SAMBLError, timings?: APITimingData }) {
     if (error || data == undefined && data !== null) {
         return (
             <ErrorPage error={error || null} />
@@ -95,14 +95,14 @@ export default function Search({ data, error }: {data?: AlbumStack | null, error
     if (data == null) {
         return (
             <>
-            <SAMBLHead
-                title = {`SAMBL • Release Seeder`}
-                fullTitle={`SAMBL MusicBrainz Release Importer`}
-            />
-            <div className="titleContainer">
-                <h1 id="searchFor">SAMBL Release Seeder</h1>
-            </div>
-            <SearchBox type="lookup" />
+                <SAMBLHead
+                    title={`SAMBL • Release Seeder`}
+                    fullTitle={`SAMBL MusicBrainz Release Importer`}
+                />
+                <div className="titleContainer">
+                    <h1 className={styles.seedTitle} id="searchFor">Seed Release</h1>
+                </div>
+                <SearchBox type="lookup" />
             </>
         )
     }
@@ -111,24 +111,29 @@ export default function Search({ data, error }: {data?: AlbumStack | null, error
     const targetAlbum = data.albums.find((match) => match.type === "target")?.album;
     return (
         <>
-            <SAMBLHead
-                title = {`SAMBL • Seed release`}
-                fullTitle={`Seed Release "${aggregatedAlbum.name}"`}
-                description={`${aggregatedAlbum.name} by ${aggregatedAlbum.artistNames.join(", ")} (${aggregatedAlbum.releaseDate}) • ${aggregatedAlbum.trackCount} tracks`}
-            />
-            <div id="err" />
-            <div className="titleContainer">
-                <h1 id="searchFor">Seed Release</h1>
-            </div>
-            <SearchBox type="lookup" />
-            <ReleaseSeedButton data={data} />
-            <br />
-            <div id="contentContainer" >
-                <div id="albumContainer" className={styles.albumContainer}>
-                    <TrackMenuInner data={data} refresh={() => {}} />
+            <div className={styles.seedPageContainer} style={{ "--background-image": `url('${aggregatedAlbum.imageUrl || ""}')` } as React.CSSProperties}>
+                <SAMBLHead
+                    title={`SAMBL • Seed release`}
+                    fullTitle={`Seed Release "${aggregatedAlbum.name}"`}
+                    description={`${aggregatedAlbum.name} by ${aggregatedAlbum.artistNames.join(", ")} (${aggregatedAlbum.releaseDate}) • ${aggregatedAlbum.trackCount} tracks`}
+                />
+                <div id="err" />
+                {/* <div className="titleContainer">
+                <h1 className={styles.seedTitle} id="searchFor">Seed Release</h1>
+            </div> */}
+                <SearchBox type="lookup" />
+                <ReleaseSeedButton data={data} />
+                <br />
+                <div id="contentContainer" >
+                    <div id="albumContainer" className={styles.albumContainer}>
+                        <div className={styles.trackBg} style={{ "--background-image": `url(${aggregatedAlbum.imageUrl})` } as React.CSSProperties} >
+                            <TrackMenuInner data={data} refresh={() => { }} isStandalone={true} />
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
+
 
     )
 }
