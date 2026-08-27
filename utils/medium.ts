91c34c75a318e6039fc50791144bae1fbf32473d
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2026-08-26 22:43:44 -0500

    [utils] Require medium format for medium.convertTrackList

--- utils/medium.ts
+++ utils/medium.ts
@@ -1,13 +1,14 @@
 import { AggregatedMedium, AggregatedTrack } from "../types/aggregated-types";
-import { MediumObject, TrackObject } from "../types/provider-types";
+import { MediumFormat, MediumObject, TrackObject } from "../types/provider-types";
 
-function convertTrackList(trackList: TrackObject[] | null): MediumObject[]
-function convertTrackList(trackList: AggregatedTrack[] | null): AggregatedMedium[]
-function convertTrackList(trackList: TrackObject[] | AggregatedTrack[] | null): MediumObject[] | AggregatedMedium[] {
+function convertTrackList(trackList: TrackObject[] | null, format: MediumFormat): MediumObject[]
+function convertTrackList(trackList: AggregatedTrack[] | null, format: MediumFormat): AggregatedMedium[]
+function convertTrackList(trackList: TrackObject[] | AggregatedTrack[] | null, format: MediumFormat): MediumObject[] | AggregatedMedium[] {
     if (!trackList || trackList.length == 0) return [];
     return [{
         tracks: trackList,
-        number: 1
+        number: 1,
+        format
     }];
 }
 
