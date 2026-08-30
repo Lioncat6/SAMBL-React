b097bae117f57f9eb5abed26ead6d451a8bfac17
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2026-08-30 15:11:34 -0500

    Update editNoteBulder for new defs

--- utils/editNoteBuilder.ts
+++ utils/editNoteBuilder.ts
@@ -1,7 +1,8 @@
-import { AggregatedAlbum, AggregatedArtist } from "../types/aggregated-types";
+import { AggregatedAlbum, AggregatedArtist, AlbumStack } from "../types/aggregated-types";
 import { DeepSearchData } from "../types/api-types";
 import { DeepSearchSelection } from "../types/component-types";
 import { ArtistObject, PartialArtistObject } from "../types/provider-types";
+import albumStack from "./albumStack";
 import text from "./text";
 
 function encode(str: string) {
@@ -32,12 +33,14 @@ function buildEditNote(edit: string, provider: string, sourceUrl: string, artist
 function buildDeepSearchEditNote(data: DeepSearchSelection): string {
     const artist = data.data.mbArtists.find(artist => artist.id == data.mbid) || data.data.mbArtists[0];
     const sourceArtist = data.data.sourceArtist;
-    
-    function getTrackArtists(album: AggregatedAlbum): PartialArtistObject[] {
-        const tracks = album.mbAlbum?.albumTracks || [];
-        const albumArtists = album.mbAlbum?.albumArtists || [];
+
+
+    function getTrackArtists(album: AlbumStack): PartialArtistObject[] {
+        const [aggregatedAlbum, sourceAlbum, targetAlbum] = albumStack.unstack(album)
+        const tracks = aggregatedAlbum?.mediums.flatMap(m => m.tracks) || [];
+        const albumArtists = aggregatedAlbum?.albumArtists || [];
         const rawArtists = tracks?.flatMap(track => track.trackArtists) || [];
-        const deDupedArtists = Array.from(new Set(rawArtists.map(artist => artist.id))).map(id => rawArtists.find(artist => artist.id === id)).filter(artist => artist).filter(artist => artist != undefined); 
+        const deDupedArtists = Array.from(new Set(rawArtists.map(artist => artist.id))).map(id => rawArtists.find(artist => artist.id === id)).filter(artist => artist).filter(artist => artist != undefined);
         if (deDupedArtists.every(artist => albumArtists.some(albumArtist => albumArtist.id === artist.id))) {
             return [];
         }
@@ -66,22 +69,30 @@ function buildDeepSearchEditNote(data: DeepSearchSelection): string {
     function getAlbumCount() {
         let count = 0
         data.data.albums.forEach((album) => {
-            if (album.mbAlbum?.albumArtists.some((aartist) => aartist.id == artist.id) || (data.trackArtists && getTrackArtists(album).some((tartist) => tartist.id == artist.id))) {
+            const [aggregatedAlbum, sourceAlbum, targetAlbum] = albumStack.unstack(album)
+            if (targetAlbum?.albumArtists.some((aartist) => aartist.id == artist.id) || (data.trackArtists && getTrackArtists(album).some((tartist) => tartist.id == artist.id))) {
                 count++;
             }
         })
         return count;
+    }
+    function generateAlbumCredits(album: AlbumStack) {
+        const [aggregatedAlbum, sourceAlbum, targetAlbum] = albumStack.unstack(album)
+        if (targetAlbum?.albumArtists && targetAlbum?.albumArtists.length > 0) {
+            return ` • '''${aggregatedAlbum.name}''' ${aggregatedAlbum.upc ? `''Barcode: ${aggregatedAlbum.upc}'' ` : ''}${aggregatedAlbum.url.url}%0A` +
+                `''Artists:'' ${targetAlbum?.albumArtists?.map(formatArtist).join(", ") || "none"}` +
+                `${data.trackArtists ? `%0A''Track Artists:'' ${getTrackArtists(album).map(formatArtist).join(", ") || "none"}` : ""}`
+        } else {
+            return undefined
+        }
     }
 
     return encode(
         `Artist matched with ''SAMBL Deep Search''%0A` +
         `'''Provider:''' ${data.data.provider}%0A` +
         `'''Albums (${getAlbumCount()}/${data.data.albums.length}):'''%0A` +
-        `${data.data.albums.map((album) => (album.mbAlbum?.albumArtists && album.mbAlbum?.albumArtists.length > 0) ? (
-            ` • '''${album.name}''' ${album.upc ? `''Barcode: ${album.upc}'' `: ''}${album.url.url}%0A`+
-            `''Artists:'' ${album.mbAlbum?.albumArtists?.map(formatArtist).join(", ") || "none"}`+
-            `${data.trackArtists ? `%0A''Track Artists:'' ${getTrackArtists(album).map(formatArtist).join(", ") || "none"}`: ""}`): undefined).filter((text) => text != undefined)
-        .join("%0A ")}%0A%0A` +
+        `${data.data.albums.map(generateAlbumCredits).filter((text) => text != undefined)
+            .join("%0A ")}%0A%0A` +
         `'''Selected Artist:''' ''${artist.name}'' | ${artist.url.url} %0A` +
         `'''Source Artist ''(${text.capitalizeFirst(sourceArtist.provider)})'':''' ''${sourceArtist.name}'' | ${sourceArtist.url.url}%0A` +
         `'''Name Similarity:''' ${text.truncateToTwo(artist.nameSimilarity * 100)}%%0A` +
@@ -92,13 +103,12 @@ function buildDeepSearchEditNote(data: DeepSearchSelection): string {
 }
 
 function buildSeedReleaseEditNote(data: AggregatedAlbum): string {
-    return encode(
-        `Release seeded from ''SAMBL''%0A` +
-        `'''Provider:''' ${data.provider}%0A` +
-        `'''Source:''' ${data.url.url}%0A` +
-        `'''Artist:''' ${data.sourceArtist?.name || "Unknown"} | ${data.sourceArtist?.url.url || "Unknown"}%0A` +
-        `%0A'''SAMBL ${process.env.NEXT_PUBLIC_VERSION}''': ${process.env.NEXT_PUBLIC_URL || "https://sambl.lioncat6.com"} | https://github.com/lioncat6/SAMBL-React`
-    );
+    return `Release seeded from ''SAMBL''\n` +
+        `'''Provider:''' ${data.provider}\n` +
+        `'''Source:''' ${data.url.url}\n` +
+        (data.sourceArtist && `'''Artist:''' ${data.sourceArtist?.name || "Unknown"} | ${data.sourceArtist?.url.url || "Unknown"}\n\n`) +
+        `'''SAMBL ${process.env.NEXT_PUBLIC_VERSION}''': ${process.env.NEXT_PUBLIC_URL || "https://sambl.lioncat6.com"} | https://github.com/lioncat6/SAMBL-React`
+    ;
 }
 
 const editNoteBuilder = {
