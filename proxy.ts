d6339fc41218dc2014f7521a6f29aac114384305
Lioncat6 <95449321+Lioncat6@users.noreply.github.com>
2026-09-23 10:53:51 -0500

    Convert proxy to ts

--- proxy.ts
+++ proxy.ts
@@ -1,11 +1,11 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 
-export function proxy(request) {
+export function proxy(request: NextRequest) {
     const url = new URL(request.url)
     const spotifyParams = ["spid", "spotifyId"]
     const spotifyRedirect = spotifyParams.find(param => url.searchParams.has(param))
     if (spotifyRedirect) {
-        url.searchParams.set('provider_id', url.searchParams.get(spotifyRedirect))
+        url.searchParams.set('provider_id', url.searchParams.get(spotifyRedirect) ?? "")
         url.searchParams.set('provider', 'spotify')
         url.searchParams.delete(spotifyRedirect)
         return NextResponse.redirect(url)
@@ -13,7 +13,7 @@ export function proxy(request) {
     const spotifyMultipleParams = ["spids", "spotifyIds"]
     const spotifyMultipleRedirect = spotifyMultipleParams.find(param => url.searchParams.has(param))
     if (spotifyMultipleRedirect) {
-        url.searchParams.set('provider_ids', url.searchParams.get(spotifyMultipleRedirect))
+        url.searchParams.set('provider_ids', url.searchParams.get(spotifyMultipleRedirect) ?? "")
         url.searchParams.set('provider', 'spotify')
         url.searchParams.delete(spotifyMultipleRedirect)
         return NextResponse.redirect(url)
